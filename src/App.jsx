8fcf835f2394f23404a7f5390dbf2d2d95c03a3a
Dee <dgrowette@gmail.com>
2022-03-08 15:20:52 -0800

    finished up sidebar menu functions

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu";
 import "./app.scss"
 import { useState } from "react";
-import Menu from "./components/menu/Menu"
+
+
+
 
 function App() {
   const [menuOpen,setMenuOpen] = useState(false)

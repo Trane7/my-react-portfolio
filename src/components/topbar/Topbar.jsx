31a41d30564c4d7c4134e4c0dc9b3111e3f25d10
Dee <dgrowette@gmail.com>
2022-03-07 18:02:35 -0800

    added to Topbar.jsx and topbar.scss files to get the header looking nice

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
 import "./topbar.scss"
+import {AccountBox, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
   return (
-    <div className= "topbar">
-
+    <div className= "topbar" id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">genius.</a>
+          <div className="itemContaner"> 
+          <AccountBox className="icon" /> {/* add icons here*/}
+          <span>612-860-9181</span>
+          </div>
+          <div className="itemContaner"> 
+          <Mail className="icon" /> {/* add icons here*/}
+          <span>dgrowette@gmail.com</span>
+          </div>
+        </div>
+        <h1>Demetre's Portfolio</h1>
+        <div className="right">
+          this is right
+        </div>
+      </div>
     </div>
   )
 }
